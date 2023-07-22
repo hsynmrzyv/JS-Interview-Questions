361ad4a3aab78eba50d50ee7996a876a2dc37005
HSYN <huseynmirzayev8@gmail.com>
2023-07-22 22:13:04 +0400

    add 2 tasks with solutions

---
+++ tasks.js
@@ -0,0 +1,15 @@
+// 1. Write a function that takes two numbers and returns the sum of those numbers
+
+const add = () => {};
+
+console.log("add", add(5, 10)); // Expects: 15
+
+////////////////////////////////////////////////////////////////
+
+// 2. Write a function that takes any number as arguments and returns the sum of all arguments
+
+const sum = () => {};
+
+console.log("sum", sum(5, 10, 15, 20)); // Expects 50
+
+////////////////////////////////////////////////////////////////
