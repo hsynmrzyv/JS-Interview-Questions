8fb2a2333cb8d9559d3273db1dc55b296a0b4bee
HSYN <huseynmirzayev8@gmail.com>
2023-09-13 19:40:23 +0400

    add tasks

--- tasks.js
+++ tasks.js
@@ -1,15 +1,23 @@
 // 1. Write a function that takes two numbers and returns the sum of those numbers
 
-const add = () => {};
-
-console.log("add", add(5, 10)); // Expects: 15
-
-////////////////////////////////////////////////////////////////
-
 // 2. Write a function that takes any number as arguments and returns the sum of all arguments
 
-const sum = () => {};
+// 3. Write a function that reverses a string.
 
-console.log("sum", sum(5, 10, 15, 20)); // Expects 50
+// 4. Write a function to calculate the factorial of a given integer.
 
-////////////////////////////////////////////////////////////////
+// 5. Write a function that prints numbers from 1 to N, but for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+
+// 6. Write a function that checks if a given number is prime?
+
+// 7. Create a function that finds the longest word in a string.
+
+// 8. Write a function that converts a sentence into title case (capitalizes the first letter of each word).
+
+// 9. Write a function that finds the second largest number in an array of numbers.
+
+// 10. Write a function that reverses the order of words in a sentence.
+
+// 11. Write a function that removes duplicate characters from a string without using additional data structures.
+
+// 12. Write a function that calculates the missing angle in a triangle when given the other two angles.
